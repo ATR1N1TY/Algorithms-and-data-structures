da1106da25e62bf061c42ff4abca0853432b97ef
unknown <saba4inasaridze@gmail.com>
2022-02-02 12:45:06 +0400

    BigO notation explained shortly  as time complexity

---
+++ DAY1.js
@@ -0,0 +1,24 @@
+//<<<<<<<<<<<<<<<<{WELCOME TO ALGORITHMS AND DATA STRUCTURES DAY1!}>>>>>>>>>>>>>>>>
+
+//1. Time complexity AKA bigO
+//2. How do we mesure code performance?
+//3. Counting operations
+//4. Big O notation introduction indepth
+
+//1.
+
+//BigO notation gives us precise numeric and objective way to judge how efficent is the code is
+
+//it helps you write better code and becomes important when number of inputs are huge like FB
+//posts, user inputs etc.
+
+//with my understanding, running inneficent code can cost too much because code takes computing
+//resources to do it's own things.
+
+//as the code coplmexity increses, consumed computing resources increase too!,
+//which means higher electricity bill and low lifespan of servers which your code runs on
+//or if you use AWS and other clouding/server services fee would be much higher than it should be!.
+
+// if you aren't building apps, you still must learn this things, because they will ask you
+//what's the time complexity of an algorithm which you wrote as they said to do it and solve it
+// on the interview.
