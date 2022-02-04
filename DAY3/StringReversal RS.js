b593a9c93003dcd5a3554f12cb62cba43e5b50c4
unknown <saba4inasaridze@gmail.com>
2022-02-04 16:10:42 +0400

    added the best solution without using push or join methods

--- DAY3/StringReversal RS.js
+++ DAY3/StringReversal RS.js
@@ -9,8 +9,25 @@ const reverseStr = (str) => {
   return res.join("");
 };
 
-const rev = reverseStr("new york");
-console.log(rev);
+// const rev = reverseStr("new york");
+// console.log(rev);
 
 //time complexity of this code is O(n)
 //space complexity of this code is O(n)
+
+//this is a second full solution,without using push and join methods
+// and it's a bit different than a first one and better.
+
+const reverseStrSec = (str) => {
+  let res = "";
+
+  for (let i = 0; i < str.length; i++) {
+    res = str[i] + res;
+  }
+  return res;
+};
+
+// const riv = reverseStrSec("nocaB .P sirhC");
+// console.log(riv);
+
+//space and time complexities are the same as in the first one: O(n)
